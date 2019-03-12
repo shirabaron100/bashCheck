fdbf58bb183144cd0e74376c2073a46bf9361720
hadar baron <44767033+hadarbaron@users.noreply.github.com>
2019-03-12 19:09:32 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
+
 folderName=$1
-executeble=$2
+executable=$2
+makeAns="pass"
+threadsAns="pass"
+memLeakAns="pass"
 curentLocation=`pwd`
+number=0
+
 cd $folderName
-num=0
-make &>/dev/null
+make 
 secssesfullMake=$?
-if [ $secssesfullMake -gt 0 ];then
-   echo "compilation
-   fail"
-    exit 7
-fi 
+ if [ $secssesfullMake -gt 0 ];  #if there is no makefile or the compilation didnt work
+  then
+    makeAns="fail";
+    echo -e "Compilation     Memory Leaks     Trhead race"
+    echo       "fail          fail              fail "
+    cd $curentLocation &>/dev/null
+    exit 7 
+else #if there is a makefile,and the compilation work
+    makeAns="pass";
 
-valgrind --tool=memcheck --leak-check=full --error-exitcode=3  ./$executeble shift 2 "$@" &>/dev/null
-sucseesfullmemory=$?
-if [ $sucseesfullmemory -gt 0 ];then 
-  smemory="fail"
-  num=$(($num+2))
-  else
-  smemory="pass"
+    #thread cheak 
+    valgrind --tool=helgrind  --error-exitcode=1 ./$executable shift 2 "$@" &>/dev/null
+    if [ $? -gt 0 ];
+     then
+        threadsAns="fail";
+        number=$(($number+1));
+        else
+            threadsAns="pass";
+
+    fi
+    # memory leak chack
+     valgrind --leak-check=full --error-exitcode=1 ./$executable shift 2 "$@" &>/dev/null
+     if [ $? -gt 0 ] ;
+     then
+        memLeakAns="fail";
+        number=$(($number+2));
+        else  memLeakAns="pass";
+    fi
 fi
+echo -e "Compilation     Memory Leaks     Trhead race"
+echo  "      "     $makeAns  "    "     $memLeakAns "          "    $threadsAns       
 
-valgrind --tool=helgrind  --error-exitcode=3  ./$executeble shift 2 "$@" &>/dev/null
-sucseesfullthread=$?
-if [ $sucseesfullthread -gt 0 ];then 
-  sthread="fail"
-  num=$(($num+1))
-  else
-  sthread="pass"
-fi
-
-  echo "compilation      memory leak       thread race
-     pass           $smemory           $sthread "
-
-cd $curentLocation  &>/dev/null
-exit $num
+cd $curentLocation &>/dev/null
+exit $number
