f9aa386b1727d8b932f815ad87e44012ce008875
shirabaron100 <44798397+shirabaron100@users.noreply.github.com>
2019-03-18 19:35:22 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
-
 folderName=$1
-executable=$2
-makeAns="pass"
-threadsAns="pass"
-memLeakAns="pass"
+executeble=$2
 curentLocation=`pwd`
-number=0
-
 cd $folderName
-make 
+num=0
+make &>/dev/null
 secssesfullMake=$?
- if [ $secssesfullMake -gt 0 ];  #if there is no makefile or the compilation didnt work
-  then
-    makeAns="fail";
-    echo -e "Compilation     Memory Leaks     Trhead race"
-    echo       "fail          fail              fail "
-    cd $curentLocation &>/dev/null
-    exit 7 
-else #if there is a makefile,and the compilation work
-    makeAns="pass";
+if [ $secssesfullMake -gt 0 ];then
+   echo "compilation
+   fail"
+    exit 7
+fi 
 
-    #thread cheak 
-    valgrind --tool=helgrind  --error-exitcode=1 ./$executable shift 2 "$@" &>/dev/null
-    if [ $? -gt 0 ];
-     then
-        threadsAns="fail";
-        number=$(($number+1));
-        else
-            threadsAns="pass";
-
-    fi
-    # memory leak chack
-     valgrind --leak-check=full --error-exitcode=1 ./$executable shift 2 "$@" &>/dev/null
-     if [ $? -gt 0 ] ;
-     then
-        memLeakAns="fail";
-        number=$(($number+2));
-        else  memLeakAns="pass";
-    fi
+valgrind --tool=memcheck --leak-check=full --error-exitcode=3  ./$executeble shift 2 "$@" &>/dev/null
+sucseesfullmemory=$?
+if [ $sucseesfullmemory -gt 0 ];then 
+  smemory="fail"
+  num=$(($num+2))
+  else
+  smemory="pass"
 fi
-echo -e "Compilation     Memory Leaks     Trhead race"
-echo  "      "     $makeAns  "    "     $memLeakAns "          "    $threadsAns       
 
-cd $curentLocation &>/dev/null
-exit $number
+valgrind --tool=helgrind  --error-exitcode=3  ./$executeble shift 2 "$@" &>/dev/null
+sucseesfullthread=$?
+if [ $sucseesfullthread -gt 0 ];then 
+  sthread="fail"
+  num=$(($num+1))
+  else
+  sthread="pass"
+fi
+
+  echo "compilation      memory leak       thread race
+     pass           $smemory           $sthread "
+
+cd $curentLocation  &>/dev/null
+exit $num  
+        
